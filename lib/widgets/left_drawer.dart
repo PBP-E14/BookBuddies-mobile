de939ff6d98688d6b709d155b900d380cce8532c
IrPanTech <irfannkamill@gmail.com>
2023-12-19 17:39:34 +0700

    add book page to left drawer

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:book_buddies_mobile/book/widgets/book_page.dart';
 import 'package:book_buddies_mobile/homepage.dart';
 import 'package:book_buddies_mobile/user/profile.dart';
 import 'package:flutter/material.dart';
@@ -26,13 +27,14 @@ class LeftDrawer extends StatelessWidget {
                   ),
                 ),
                 Padding(padding: EdgeInsets.all(10)),
-                Text("Book Buddies!",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Color.fromRGBO(255, 205, 210, 1),
-                    ),
-                    ),
+                Text(
+                  "Book Buddies!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color.fromRGBO(255, 205, 210, 1),
+                  ),
+                ),
               ],
             ),
           ),
@@ -45,6 +47,18 @@ class LeftDrawer extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) => MyHomePage(),
+                  ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.menu_book_rounded),
+            title: const Text('Book Page'),
+            // Bagian redirection ke ListBookPage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const BookPage(),
                   ));
             },
           ),
