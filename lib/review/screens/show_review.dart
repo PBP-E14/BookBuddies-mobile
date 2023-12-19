bfd59bcfd8962cfca0b8a4ec441062fd99c6b4ed
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2023-12-19 12:43:28 +0700

    review udh fix jadi udh muncul datanya di page

--- lib/review/screens/show_review.dart
+++ lib/review/screens/show_review.dart
@@ -33,57 +33,58 @@ class _ShowReviewState extends State<ShowReview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Review'),
-        ),
-        drawer: const LeftDrawer(),
-        body: Expanded(
-          child: FutureBuilder(
-              future: fetchProduct(),
-              builder: (context, AsyncSnapshot snapshot) {
-                if (snapshot.data == null) {
-                  return const Center(child: CircularProgressIndicator());
-                } else {
-                  if (!snapshot.hasData) {
-                    return const Column(
-                      children: [
-                        Text(
-                          "No book has been reviewed yet",
-                          style:
-                              TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+      appBar: AppBar(
+        title: const Text('Review'),
+      ),
+      drawer: const LeftDrawer(),
+      body: FutureBuilder(
+        future: fetchProduct(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (!snapshot.hasData) {
+              return const Column(
+                children: [
+                  Text(
+                    "No book has been reviewed yet",
+                    style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index) => Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${snapshot.data![index].fields.date}, created by ${snapshot.data![index].fields.user}",
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
                         ),
-                        SizedBox(height: 8),
-                      ],
-                    );
-                  } else {
-                    return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (_, index) => Container(
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 16, vertical: 12),
-                              padding: const EdgeInsets.all(20.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "${snapshot.data![index].fields.date}, created by ${snapshot.data![index].fields.user}",
-                                    style: const TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text("${snapshot.data![index].fields.title}"),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                      "${snapshot.data![index].fields.description.review}")
-                                ],
-                              ),
-                            ));
-                  }
-                }
-              }),
-        ));
+                      ),
+                      const SizedBox(height: 10),
+                      Text("${snapshot.data![index].fields.title}"),
+                      const SizedBox(height: 10),
+                      Text("${snapshot.data![index].fields.review}")
+                    ],
+                  ),
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
   }
 }
