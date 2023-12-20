e704cf883723edeb0c1a3db7593287367ecbcd7d
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2023-12-20 13:09:07 +0700

    form udh jadi, tinggal filter sama design

---
+++ lib/review/screens/review_form.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+
+import 'package:book_buddies_mobile/review/screens/show_review.dart';
+import 'package:book_buddies_mobile/widgets/left_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+class ReviewForm extends StatefulWidget {
+  const ReviewForm({super.key});
+
+  @override
+  State<ReviewForm> createState() => _ReviewFormState();
+}
+
+class _ReviewFormState extends State<ReviewForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = '';
+  String _review = '';
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Add Review',
+          ),
+        ),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      drawer: const LeftDrawer(),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Book Title",
+                  labelText: "Book Title",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _title = value!;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Title can't be empty!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Review",
+                  labelText: "Review",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _review = value!;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Review content can't be empty!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.indigo),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      final response = await request.postJson(
+                          "http://127.0.0.1:8000/review/create-flutter/",
+                          jsonEncode(<String, String>{
+                            'title': _title,
+                            'review': _review,
+                          }));
+                      if (response['status'] == 'success') {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("Review has been added successfully"),
+                        ));
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => ShowReview()),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content:
+                              Text("Terdapat kesalahan, silakan coba lagi."),
+                        ));
+                      }
+                    }
+                  },
+                  child: const Text(
+                    "Save",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
--- lib/review/screens/show_review.dart
+++ lib/review/screens/show_review.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:book_buddies_mobile/review/screens/review_form.dart';
 import 'package:flutter/material.dart';
 import 'package:book_buddies_mobile/widgets/left_drawer.dart';
 import 'package:http/http.dart' as http;
@@ -11,9 +12,10 @@ class ShowReview extends StatefulWidget {
   State<ShowReview> createState() => _ShowReviewState();
 }
 
-class _ShowReviewState extends State<ShowReview> {
+class _ShowReviewState extends State<ShowReview>
+    with SingleTickerProviderStateMixin {
   Future<List<Review>> fetchProduct() async {
-    var url = Uri.parse('http://localhost:8000/review/show_json_review/');
+    var url = Uri.parse('http://127.0.0.1:8000/review/show_json_review/');
     var response = await http.get(
       url,
       headers: {"Content-Type": "application/json"},
@@ -34,56 +36,92 @@ class _ShowReviewState extends State<ShowReview> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Review'),
+        title: const Text('Reviews'),
       ),
       drawer: const LeftDrawer(),
       body: FutureBuilder(
         future: fetchProduct(),
         builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else {
-            if (!snapshot.hasData) {
-              return const Column(
-                children: [
-                  Text(
-                    "No book has been reviewed yet",
-                    style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                  ),
-                  SizedBox(height: 8),
-                ],
+            if (!snapshot.hasData || snapshot.data.isEmpty) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "No reviews available yet",
+                      style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                    ),
+                    SizedBox(height: 8),
+                  ],
+                ),
               );
             } else {
               return ListView.builder(
                 itemCount: snapshot.data!.length,
-                itemBuilder: (_, index) => Container(
+                itemBuilder: (_, index) => Card(
+                  elevation: 4,
                   margin: const EdgeInsets.symmetric(
                     horizontal: 16,
                     vertical: 12,
                   ),
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${snapshot.data![index].fields.date}, created by ${snapshot.data![index].fields.user}",
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${snapshot.data![index].fields.date}",
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text("${snapshot.data![index].fields.title}"),
-                      const SizedBox(height: 10),
-                      Text("${snapshot.data![index].fields.review}")
-                    ],
+                        const SizedBox(height: 8),
+                        Text(
+                          "Created by: ${snapshot.data![index].fields.user}",
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          "${snapshot.data![index].fields.title}",
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          "${snapshot.data![index].fields.review}",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
             }
           }
         },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: SizedBox(
+        width: 80.0, // Adjust the width
+        height: 50.0, // Adjust the height
+        child: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => ReviewForm()));
+          },
+          child: const Icon(Icons.add, size: 40.0), // Adjust the icon size
+          tooltip: "Add Review",
+        ),
       ),
     );
   }
